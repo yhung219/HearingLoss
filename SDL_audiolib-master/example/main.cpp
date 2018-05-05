60308b7026f37a88d0b98f90d46511b006b4821e
yhung219 <yvonne.hung@network.rca.ac.uk>
2018-05-05 19:43:33 +0100

    Added SDL resampler library

---
+++ SDL_audiolib-master/example/main.cpp
@@ -0,0 +1,40 @@
+#include "Aulib/AudioStream.h"
+#include "Aulib/AudioResamplerSpeex.h"
+#include "Aulib/AudioDecoderSndfile.h"
+#include "Aulib/AudioDecoderFluidsynth.h"
+#include "Aulib/AudioDecoderWildmidi.h"
+#include "Aulib/AudioDecoderBassmidi.h"
+#include <memory>
+#include <SDL.h>
+#include <iostream>
+//#include "filedetector.h"
+
+using namespace Aulib;
+
+int main(int /*argc*/, char* argv[])
+{
+    init(44100, AUDIO_S16SYS, 2, 4096);
+
+    AudioDecoderBassmidi::setDefaultSoundfont("/usr/local/share/soundfonts/gs.sf2");
+    AudioDecoderWildmidi::init("/usr/share/timidity/current/timidity.cfg", 44100, true, true);
+
+    auto decoder = AudioDecoder::decoderFor(argv[1]);
+    if (decoder == nullptr) {
+        std::cerr << "No decoder found.\n";
+        return 1;
+    }
+
+
+    auto fsynth = dynamic_cast<AudioDecoderFluidSynth*>(decoder.get());
+    //auto bassmidi = dynamic_cast<AudioDecoderBassmidi*>(decoder.get());
+    if (fsynth != nullptr) {
+        fsynth->loadSoundfont("/usr/local/share/soundfonts/gs.sf2");
+    }
+
+    AudioStream stream(argv[1], std::move(decoder), std::make_unique<AudioResamplerSpeex>());
+    stream.play();
+    while (stream.isPlaying()) {
+        SDL_Delay(200);
+    }
+    Aulib::quit();
+}
