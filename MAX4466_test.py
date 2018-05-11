5f63a2099db95d353efe18fe940cf5f74aaa4fd3
yhung219 <yvonne.hung@network.rca.ac.uk>
2018-05-11 19:54:23 +0100

    debug MAX4466_test.py

--- MAX4466_test.py
+++ MAX4466_test.py
@@ -26,6 +26,7 @@ def getAdc(channel):
 
     # Preform SPI transaction and store returned bits in 'r'
     r = spi.xfer([1, (8 + channel) << 4, 0])
+    print (r)
 
     # Filter data bits from returned bits
     adcOut = ((r[1] & 3) << 8) + r[2]
